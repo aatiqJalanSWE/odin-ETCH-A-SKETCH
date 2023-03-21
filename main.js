4c3edf81ca05e26737f21010dfef039275b5f970
aatiqJalanSWE <aatiq.afzal@jalantechnologies.com>
2023-03-22 02:37:56 +0530

    Fixed

--- main.js
+++ main.js
@@ -1,56 +1,61 @@
 let color = "black";
 
-document.addEventListener("DOMContentLoaded", function() {
-    createBoard(16);
-    let btn_popup = document.querySelector("#popup");
-    btn_popup.addEventListener("click", function() {
-        let size = getSize();
-        createBoard(size);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  createBoard(16);
+
+  const btnPopup = document.querySelector("#popup");
+
+  btnPopup.addEventListener("click", function () {
+    const size = getSize();
+    createBoard(size);
+  });
 });
 
 function createBoard(size) {
-    let board = document.querySelector(".board");
+  const board = document.querySelector(".board");
 
-    board.style.gridTemplateColumns = `repeat(${size},1fr)`;
-    board.style.gridTemplateRows = `repeat(${size},1fr)`;
+  board.style.gridTemplateColumns = `repeat(${size},1fr)`;
+  board.style.gridTemplateRows = `repeat(${size},1fr)`;
 
-    let numDivs = size * size;
+  const numDivs = size * size;
 
-    for (let i = 0; i < numDivs; i++) {
-        let div = document.createElement("div");
-        div.addEventListener("mouseover", colorDiv);
+  for (let i = 0; i < numDivs; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mouseover", colorDiv);
 
-        board.insertAdjacentElement("beforeend", div);
-    }
+    board.insertAdjacentElement("beforeend", div);
+  }
 }
 
 function getSize() {
-    let input = prompt("Size of the board");
-    let message = document.querySelector("#text");
-    if (input == "") {
-        message.innerHTML = "Please provide a number";
-    } else if (input < 0 || input > 100) {
-        message.innerHTML = "Provide a number between 1 and 100";
-    } else {
-        message.innerHTML = "Now you can play!";
-        return input;
-    }
+  const input = prompt("Size of the board");
+
+  const message = document.querySelector("#text");
+
+  if (input == "") {
+    message.innerHTML = "Please provide a number";
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = "Provide a number between 1 and 100";
+  } else {
+    message.innerHTML = "Now you can play!";
+    return input;
+  }
 }
 
 function colorDiv() {
-    if (color == "random") {
-        this.style.backgroundColor = `hsl(${Math.random() * 360} , 100% , 50%)`;
-    } else {
-        this.style.backgroundColor = "black";
-    }
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360} , 100% , 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
 }
 
 function setColor(colorChoice) {
-    color = colorChoice;
+  color = colorChoice;
 }
 
 function resetBoard() {
-    let divs = document.querySelectorAll("div");
-    divs.forEach((div) => div.style.backgroundColor = "white");
+  const divs = document.querySelectorAll("div");
+
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
 }
